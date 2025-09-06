422327db154727a9c5b7b0a2385d91c01210d10a
Gareth Hughes <hughesrgareth@proton.me>
2025-09-06 20:47:52 +1000

    Half arsed pass at styling

--- assets/js/faq.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('details').forEach(d => {
-  d.addEventListener('toggle', () => {
-    if (d.open) {
-      document.querySelectorAll('details[open]').forEach(other => {
-        if (other !== d) other.removeAttribute('open');
-      });
-    }
-  });
-});
